edcd5652adab82a1265f1e742dd62a48b79202c3
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-13 19:13:44 +0000

    tail commit

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+}
+
+const tail = function (arr) {
+  return arr.slice(1)
+}
