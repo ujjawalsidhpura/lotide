141ff4c7775bf20972c9f708e9ef04f5f1c8be9e
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-13 18:48:06 +0000

    satisfied

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+let assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+}
+
+assertEqual('uj', 'uj');
