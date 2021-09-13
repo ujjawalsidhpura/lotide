5f4da2ed49570b5c9f682ad7e149f7710e43682a
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-13 19:03:31 +0000

    head changes

---
+++ head.js
@@ -0,0 +1,12 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+}
+
+const head = function (arr, headElement) {
+  return arr[0];
+}
+
