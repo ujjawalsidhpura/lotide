7a4bbba358ea70c4d73e4ee7026f6765d889c4ec
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-15 22:10:45 +0000

    map

---
+++ takeUntil.js
@@ -0,0 +1,28 @@
+
+const eqArrays = function (arr1, arr2) {
+  let response = true;
+  if (arr1.length !== arr2.length) {
+    response = false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      response = false;
+    }
+  }
+  return response;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  return eqArrays(arr1, arr2);
+}
+
+const takeUntil = function (array, callback) {
+  let output = array.map(callback)
+  return output
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+console.log(results1);
