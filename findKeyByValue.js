1bf969825d868f6aa04b603536abe6efb7e2ad44
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-15 15:48:48 +0000

    new function key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+//Main Function for this exercise
+
+const findKeyByValue = function (obj, val) {
+
+  for (let genre in obj) {
+    if (obj[genre] === val) {
+      return genre
+    }
+  }
+}
+
+//Assert Function 
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
