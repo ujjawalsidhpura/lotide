1e2b92de9adfbad7411a50411c64168c2f321334
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-15 21:46:13 +0000

    map

---
+++ map.js
@@ -0,0 +1,11 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const result = [];
+
+  for (let item of array) {
+    result.push(callback(item));
+  }
+
+  return result;
+}
