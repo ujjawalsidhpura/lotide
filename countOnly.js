a2bf12299dd08f23651f3fee1731245318bd53cc
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-15 15:06:24 +0000

    couting in object function prac

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} === ${expected}`);
+  }
+}
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+//Code ends here.....
+
+//Testing below
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const obj = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }
+
+//personal test case
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+//personla test case
+console.log(countOnly(firstNames, obj))
