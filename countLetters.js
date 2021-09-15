17c67a674d271aec2a65737849709950b79442c5
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-15 15:11:43 +0000

    new function to count letters

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function (str) {
+  let count = {};
+  let string = str.split('');
+
+  for (let letter of string) {
+    if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  return count;
+}
